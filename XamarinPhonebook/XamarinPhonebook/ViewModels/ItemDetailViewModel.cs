8c1e78d91f13a8f7fcd6038d3ad35b6d33d6aa47
Mifort <themifort@gmail.com>
2019-11-07 14:15:44 +0300

    Добавьте файлы проекта.

---
+++ XamarinPhonebook/XamarinPhonebook/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinPhonebook.Models;
+
+namespace XamarinPhonebook.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
