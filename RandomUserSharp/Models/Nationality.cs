52bddd562736a3948fd7912e18d9e30e8477bd8c
Mifort <themifort@gmail.com>
2019-11-07 17:04:50 +0300

    Added RandomUser sharp wrapper with fixes. Based on https://github.com/tsjdev-apps/RandomUserSharp

---
+++ RandomUserSharp/Models/Nationality.cs
@@ -0,0 +1,25 @@
+﻿namespace RandomUserSharp.Models
+{
+    public enum Nationality
+    {
+        ALL,
+        AU,
+        BR,
+        CA,
+        CH,
+        DE,
+        DK,
+        ES,
+        FI,
+        FR,
+        GB,
+        IE,
+        IR,
+        NO,
+        NL,
+        NZ,
+        TR,
+        US,
+        LEGO
+    }
+}
